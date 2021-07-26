8b90df3177f684ad25e840c6697f400ba256b287
liangxiao6 <liangxiao6@live.com>
2021-07-26 13:26:15 +0800

    merger

--- commonlibs/yewulibs/slbcitypickerview/src/main/java/com/lljjcoder/bean/CityJson.java
+++
@@ -1,4 +0,0 @@
-package com.lljjcoder.bean;
-
-public class CityJson {
-}
--- commonlibs/zhibopolyv/polyvLiveCommonModul/src/main/java/com/easefun/polyv/livecommon/module/utils/PLVFileProvider.java
+++
@@ -1,6 +0,0 @@
-package com.easefun.polyv.livecommon.module.utils;
-
-import androidx.core.content.FileProvider;
-
-public class PLVFileProvider extends FileProvider {
-}
