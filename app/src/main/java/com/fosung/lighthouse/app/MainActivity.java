faa01944e7dcfe40611428db309e331fcf5f77fe
liangxiao6 <liangxiao6@live.com>
2021-08-17 17:15:25 +0800

    merger

--- app/src/main/java/com/fosung/lighthouse/app/MainActivity.java
+++ app/src/main/java/com/fosung/lighthouse/app/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.haier.cellarette.libwebview.DemoWebviewMainActivity;
+import com.geek.libxuanzeqi.DateTimePickerActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(this, DemoWebviewMainActivity.class));
+        startActivity(new Intent(this, DateTimePickerActivity.class));
     }
 }
