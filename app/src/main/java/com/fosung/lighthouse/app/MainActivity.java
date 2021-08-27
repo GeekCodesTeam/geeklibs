ebf4cec26a9af0097518c0854eb77153e9720b9d
liangxiao6 <liangxiao6@live.com>
2021-08-27 19:18:15 +0800

    merge

--- app/src/main/java/com/fosung/lighthouse/app/MainActivity.java
+++ app/src/main/java/com/fosung/lighthouse/app/MainActivity.java
@@ -1,11 +1,8 @@
 package com.fosung.lighthouse.app;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.geek.libxuanzeqi.DateTimePickerActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(this, DateTimePickerActivity.class));
+//        startActivity(new Intent(this, DateTimePickerActivity.class));
     }
 }
