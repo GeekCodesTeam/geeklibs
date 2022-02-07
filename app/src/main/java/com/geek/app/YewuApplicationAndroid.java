6cd70355dc3fd305cfb2cff65cea7b84410ccd37
liangxiao6 <liangxiao6@live.com>
2022-02-07 14:24:15 +0800

    merger

--- app/src/main/java/com/geek/app/YewuApplicationAndroid.java
+++ app/src/main/java/com/geek/app/YewuApplicationAndroid.java
@@ -1,8 +1,12 @@
 package com.geek.app;
 
+import android.content.Intent;
+
 import com.geek.libbase.AndroidApplication;
 import com.geek.libutils.app.AppUtils;
 import com.geek.libutils.data.MmkvUtils;
+import com.github.anrwatchdog.ANRError;
+import com.github.anrwatchdog.ANRWatchDog;
 
 public class YewuApplicationAndroid extends AndroidApplication {
 
@@ -21,6 +25,19 @@ public class YewuApplicationAndroid extends AndroidApplication {
         others();
         //TODO 业务bufen
         MmkvUtils.getInstance().set_xiancheng("App.isLogined", false);
+        //
+        new ANRWatchDog()
+                .setANRListener(new ANRWatchDog.ANRListener() {
+                    @Override
+                    public void onAppNotResponding(ANRError error) {
+                        Intent intent = getPackageManager()
+                                .getLaunchIntentForPackage(getPackageName());
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+                        android.os.Process.killProcess(android.os.Process.myPid());
+                    }
+                })
+                .start();
     }
 
     @Override
